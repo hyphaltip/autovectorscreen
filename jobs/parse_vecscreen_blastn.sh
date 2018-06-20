bcf3c23861baf065cbc1d2e33682e840b96b6e85
Jason Stajich <jasonstajich.phd@gmail.com>
2018-06-20 09:01:40 -0700

    process blast or ncbi reports for screening vector and contam

--- jobs/parse_vecscreen_blastn.sh
+++ jobs/parse_vecscreen_blastn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #SBATCH --ntasks 6 --nodes 1 --mem 8G --out parse.log -J cleanvectparse  -p short 
-
+module load python/3
 # fix this if installed do not need a relative folder
-parallel -j 6 ./scripts/clean_vect_blastn.py {} {}.vecscreen.blastn ::: *Trinity_Assembly.fasta
+parallel -j 6 ./scripts/clean_vect_blastn.py {} {}.vecscreen.blastn ::: *.fasta
--- jobs/vecscreen_blastn.sh
+++ jobs/vecscreen_blastn.sh
@@ -5,9 +5,9 @@ module load ncbi-blast
 
 # NCBI parameters for vector screen, doing this with parallel for now
 
-JOBS=4
-CPUS_PER_JOB=4
-DB=UniVec
+JOBS=2
+CPUS_PER_JOB=8
+DB=/srv/projects/db/UniVec/UniVec
 
 parallel -j $JOBS blastn -task blastn -reward 1 -penalty -5 -gapopen 3 -gapextend 3 -dust yes -soft_masking true -evalue 700 -searchsp 1750000000000 \
- -db $DB -outfmt 6 -num_threads $CPUS_PER_JOB -query {} -out {}.vecscreen.blastn ::: *Trinity_Assembly.fasta
+ -db $DB -outfmt 6 -num_threads $CPUS_PER_JOB -query {} -out {}.vecscreen.blastn ::: *.fasta
